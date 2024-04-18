9f332f8a458debc0359246d13826217ce7a4e721
Lwall02 <liam.wall@mail.utoronto.ca>
2024-04-18 11:20:51 -0400

    Update model

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -14,15 +14,13 @@ library(testthat)
 #### Test data ####
 all_data <- read_csv("outputs/data/all_merged_data.csv")
 
-# Assuming your dataframe is named 'all_data' and is already in your environment
-
 # Test to check if 'year' is a factor
 test_that("Year variable is a factor", {
   expect_true(is.factor(all_data$year), info = "The year column should be a factor.")
 })
 
 # Tests to check if other variables are numeric and not NA
-variables_to_check <- c("wildfire_area", "annual_co2_emissions_per_capita", 
+variables_to_check <- c("wildfire_area", "annual_co_emissions_per_capita", 
                         "average_temp", "global_population", "gmsl")
 
 for (var in variables_to_check) {
@@ -32,22 +30,10 @@ for (var in variables_to_check) {
   
   test_that(paste(var, "has no missing values"), {
     expect_false(any(is.na(all_data[[var]])), info = paste(var, "should not contain NA values."))
+    
+  test_that(paste(var, "values are positive"), {
+    expect_true(all(all_data[[var]] > 0), info = "Wildfire area values should be positive.")
+    })
   })
   
-  # If you have specific numeric ranges or values for each variable, you can add additional tests here.
-  # For example, if you know that 'wildfire_area' should always be positive, you could add:
-  # test_that(paste(var, "values are positive"), {
-  #   expect_true(all(all_data$wildfire_area > 0), info = "Wildfire area values should be positive.")
-  # })
-  
-  # Add more tests as necessary for each variable...
 }
-
-# Optionally, you can specify known ranges or specific values for your variables.
-# For example:
-# test_that("wildfire_area values are within expected range", {
-#   expect_true(all(all_data$wildfire_area >= 0), info = "Wildfire areas should be non-negative.")
-# })
-
-# Run the tests
-test_dir("tests")
